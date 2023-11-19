02c4cc55d6da57165923255821fd4d9695205335
mailynfz <96663365+mailynfz@users.noreply.github.com>
2023-11-19 15:37:53 -0500

    Add files via upload

---
+++ public-app.py
@@ -0,0 +1,128 @@
+
+import openai
+import streamlit as st
+import time
+
+# Set OpenAI contants 
+if st.secrets:
+    if 'ASSISTANT_ID' in st.secrets:
+        assistant_id = st.secrets['ASSISTANT_ID']
+
+    if 'OPENAI_API_KEY' in st.secrets:
+        st.success('API key already provided!', icon='✅')
+        openai.api_key = st.secrets['OPENAI_API_KEY']
+    
+
+# Initialize the OpenAI client
+client = openai
+
+# Define the function to process messages with citations
+def process_message_with_citations(message):
+    """Extract content and annotations from the message and format citations as footnotes."""
+    message_content = message.content[0].text
+    annotations = message_content.annotations if hasattr(message_content, 'annotations') else []
+    citations = []
+
+    # Iterate over the annotations and add footnotes
+    for index, annotation in enumerate(annotations):
+        # Replace the text with a footnote
+        message_content.value = message_content.value.replace(annotation.text, f' [{index + 1}]')
+
+        # Gather citations based on annotation attributes
+        if (file_citation := getattr(annotation, 'file_citation', None)):
+            # Retrieve the cited file details (dummy response here since we can't call OpenAI)
+            cited_file = {'filename': 'cited_document.pdf'}  # This should be replaced with actual file retrieval
+            citations.append(f'[{index + 1}] {file_citation.quote} from {cited_file["filename"]}')
+        elif (file_path := getattr(annotation, 'file_path', None)):
+            # Placeholder for file download citation
+            cited_file = {'filename': 'downloaded_document.pdf'}  # This should be replaced with actual file retrieval
+            citations.append(f'[{index + 1}] Click [here](#) to download {cited_file["filename"]}')  # The download link should be replaced with the actual download path
+
+    # Add footnotes to the end of the message content
+    full_response = message_content.value + '\n\n' + '\n'.join(citations)
+    return full_response
+
+
+# Initialize session state variables for file IDs and chat control
+if "file_id_list" not in st.session_state:
+    st.session_state.file_id_list = []
+
+if "start_chat" not in st.session_state:
+    st.session_state.start_chat = False
+
+if "thread_id" not in st.session_state:
+    st.session_state.thread_id = None
+
+# Set up the Streamlit page with a title and icon
+st.set_page_config(page_title="ACIS PhD GPT", page_icon=":speech_balloon:")
+
+# Main chat interface setup
+st.title("ACIS PhD GPT")
+st.write("GPT-Powered Chat Assistant for the ACIS PhD Program at Virginia Tech's Pamplin School of Business")
+
+# Initiate Chat
+st.session_state.start_chat = True
+
+# Create a thread once and store its ID in session state
+thread = client.beta.threads.create()
+st.session_state.thread_id = thread.id
+st.write("thread id: ", thread.id)
+
+# Only show the chat interface if the chat has been started
+if st.session_state.start_chat:
+    # Initialize the model and messages list if not already in session state
+    if "openai_model" not in st.session_state:
+        st.session_state.openai_model = "gpt-4-1106-preview"
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Display existing messages in the chat
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # Chat input for the user
+    if prompt := st.chat_input("What's up, doc?"):
+        # Add user message to the state and display it
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        # Add the user's message to the existing thread
+        client.beta.threads.messages.create(
+            thread_id=st.session_state.thread_id,
+            role="user",
+            content=prompt
+        )
+
+        # Create a run with additional instructions
+        run = client.beta.threads.runs.create(
+            thread_id=st.session_state.thread_id,
+            assistant_id=assistant_id
+        )
+
+        # Poll for the run to complete and retrieve the assistant's messages
+        while run.status != 'completed':
+            time.sleep(.5)
+            run = client.beta.threads.runs.retrieve(
+                thread_id=st.session_state.thread_id,
+                run_id=run.id
+            )
+
+        # Retrieve messages added by the assistant
+        messages = client.beta.threads.messages.list(
+            thread_id=st.session_state.thread_id
+        )
+
+        # Process and display assistant messages
+        assistant_messages_for_run = [
+            message for message in messages 
+            if message.run_id == run.id and message.role == "assistant"
+        ]
+        for message in assistant_messages_for_run:
+            full_response = process_message_with_citations(message)
+            st.session_state.messages.append({"role": "assistant", "content": full_response})
+            with st.chat_message("assistant"):
+                st.markdown(full_response, unsafe_allow_html=True)
+else:
+    st.write("Something is wrong. Please try again later.")
